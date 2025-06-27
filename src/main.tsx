f0d5078e7a26b218c5083449042fd5ae7418d40f
HaMeleonchik <tntshow.tiktok@gmail.com>
2025-06-27 17:00:25 +0300

    add-modern-normalize

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react';
 import App from './components/App/App.tsx'
+import 'modern-normalize/modern-normalize.css';
 import './index.css'
 
 createRoot(document.getElementById('root') as HTMLDivElement).render(
